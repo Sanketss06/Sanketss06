8d15e3e3ab12b374fcce5d0765dbc3ff8eea0d4f
Sanketss06 <99062285+Sanketss06@users.noreply.github.com>
2022-02-12 09:51:22 +0530

    Create Sankat.cs

---
+++ Sankat.cs
@@ -0,0 +1,28 @@
+- 👋 Hi, I’m @Sanketss06
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+Sanketss06/Sanketss06 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+static void Main(string[] args)
+{
+Consol.WriteLine("hello");
+Console.ReadLine();
+}
+    }
+}
